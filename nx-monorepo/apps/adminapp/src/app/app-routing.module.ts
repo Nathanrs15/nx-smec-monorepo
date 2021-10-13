db1bf3c25ba452b13946e4a0b3ce35825087df3a
Jonathan Rojas Sandoval <jonathan15rojas@gmail.com>
2021-10-13 18:08:50 +0200

    Docker + Nginx archivos de conf

--- nx-monorepo/apps/adminapp/src/app/app-routing.module.ts
+++ nx-monorepo/apps/adminapp/src/app/app-routing.module.ts
@@ -1,76 +1,82 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {
-  AuthenticationGuard,
-  UserIsLoggedGuard,
+    AuthenticationGuard,
+    UserIsLoggedGuard,
 } from '@smec-monorepo/core-app';
 
 import { LayoutComponent } from '@smec-monorepo/layout';
 import { InitialDataResolver } from './app.resolvers';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: LayoutComponent,
-    data: {
-      layout: 'empty',
-    },
-    children: [
-      {
-        path: 'sign-in',
-        canActivate: [UserIsLoggedGuard],
-        loadChildren: () =>
-          import('@smec-monorepo/modules/auth').then((m) => m.AuthSignInModule),
-      },
-      {
-        path: 'sign-out',
-        loadChildren: () =>
-          import('@smec-monorepo/modules/auth').then(
-            (m) => m.AuthSignOutModule
-          ),
-      },
-    ],
-  },
-  {
-    path: 'app',
-    component: LayoutComponent,
-    // data: {
-    //   layout: 'classic',
+    // {
+    //     path: '',
+    //     component: LayoutComponent,
+    //     data: {
+    //         layout: 'empty',
+    //     },
+    //     children: [
+    //         {
+    //             path: 'sign-in',
+    //             canActivate: [UserIsLoggedGuard],
+    //             loadChildren: () =>
+    //                 import('@smec-monorepo/modules/auth').then(
+    //                     (m) => m.AuthSignInModule
+    //                 ),
+    //         },
+    //         {
+    //             path: 'sign-out',
+    //             loadChildren: () =>
+    //                 import('@smec-monorepo/modules/auth').then(
+    //                     (m) => m.AuthSignOutModule
+    //                 ),
+    //         },
+    //     ],
     // },
-    canActivate: [AuthenticationGuard],
-
-    resolve: {
-      initialData: InitialDataResolver,
+    {
+        path: 'test',
+        component: LayoutComponent,
+        data: {
+            layout: 'empty',
+        },
     },
-    children: [
-      {
-        path: 'users',
-        loadChildren: () =>
-          import('@smec-monorepo/modules/users').then(
-            (m) => m.UsersShellWebModule
-          ),
-      },
-      {
-        path: 'roles',
-        loadChildren: () =>
-          import('@smec-monorepo/modules/roles').then(
-            (m) => m.RolesShellWebModule
-          ),
-      },
-      {
-        path: 'history-graph',
-        loadChildren: () =>
-          import('@smec-monorepo/modules/cems/history-graph').then(
-            (m) => m.HistoryGraphShellWebModule
-          ),
-      },
-    ],
-  },
-  { path: '**', redirectTo: 'app', pathMatch: 'full' },
+    /*{
+        path: 'app',
+        component: LayoutComponent,
+         canActivate: [AuthenticationGuard],
+
+        resolve: {
+            initialData: InitialDataResolver,
+        },
+        children: [
+            {
+                path: 'users',
+                loadChildren: () =>
+                    import('@smec-monorepo/modules/users').then(
+                        (m) => m.UsersShellWebModule
+                    ),
+            },
+            {
+                path: 'roles',
+                loadChildren: () =>
+                    import('@smec-monorepo/modules/roles').then(
+                        (m) => m.RolesShellWebModule
+                    ),
+            },
+            {
+                path: 'history-graph',
+                loadChildren: () =>
+                    import('@smec-monorepo/modules/cems/history-graph').then(
+                        (m) => m.HistoryGraphShellWebModule
+                    ),
+            },
+        ],
+    },*/
+    { path: '**', redirectTo: 'test', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
